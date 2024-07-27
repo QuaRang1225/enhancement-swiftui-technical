d4535e791d7d4299691a8117975c1b87337c1225
QuaRang <dbduddnd1225@gmail.com>
2024-07-27 14:46:05 +0900

    GeometryReader 예제 작성

--- GeometryReader/GeomertyReaderView.swift
+++ GeometryReader/GeomertyReaderView.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct GeomertyReaderView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            VStack(spacing:0){
+                Circle()
+                    .frame(width: 100,height: 100)
+                GeometryReader{ geometry in
+                    let globalFrame = geometry.frame(in: .global)
+                    let localFrame = geometry.frame(in: .local)
+                    let nameFrame = geometry.frame(in: .named("GEO"))
+                    VStack(alignment:.leading){
+                        Text("글로벌 : \(globalFrame.midX) \(globalFrame.minY)")
+                        Text("로컬 : \(localFrame.midX) \(localFrame.minY)")
+                        Text("네임 : \(nameFrame.midX) \(nameFrame.minY)")
+                        Text("디바이스 : \(mainWidth) \(mainHeight)")
+                    }
+                }
+            }
+        }
+        .coordinateSpace(name: "GEO")
     }
 }
 
 #Preview {
     GeomertyReaderView()
 }
+
+
+
