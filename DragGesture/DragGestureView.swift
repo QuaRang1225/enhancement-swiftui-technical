75458a10bca5d30cb53754e73e9747e88220398d
QuaRang <dbduddnd1225@gmail.com>
2024-07-27 18:25:04 +0900

    DragGestureView 생성

---
+++ DragGesture/DragGestureView.swift
@@ -0,0 +1,18 @@
+//
+//  DragGesture.swift
+//  TechnicalUI
+//
+//  Created by 유영웅 on 7/27/24.
+//
+
+import SwiftUI
+
+struct DragGestureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DragGestureView()
+}
