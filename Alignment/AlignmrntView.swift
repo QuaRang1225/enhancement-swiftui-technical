ffa0713cf705d2aed388e82f487b398b5da12cdf
QuaRang <dbduddnd1225@gmail.com>
2024-08-01 20:30:55 +0900

    파일 추가

---
+++ Alignment/AlignmrntView.swift
@@ -0,0 +1,18 @@
+//
+//  AlignmrntView.swift
+//  TechnicalUI
+//
+//  Created by 유영웅 on 8/1/24.
+//
+
+import SwiftUI
+
+struct AlignmrntView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlignmrntView()
+}
---
+++ FlowLayout/FlowLayoutView.swift
@@ -0,0 +1,18 @@
+//
+//  FlowLayoutView.swift
+//  TechnicalUI
+//
+//  Created by 유영웅 on 8/1/24.
+//
+
+import SwiftUI
+
+struct FlowLayoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlowLayoutView()
+}
