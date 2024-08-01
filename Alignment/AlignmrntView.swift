9348e83da7b401b2c7ab162908a3a1b8a9875b6f
QuaRang <dbduddnd1225@gmail.com>
2024-08-01 20:31:29 +0900

    - SwiftUI alignmentGuide 관련 예제 작성 - 조금 더 연구할 필요가 있어보임

--- Alignment/AlignmrntView.swift
+++ Alignment/AlignmrntView.swift
@@ -9,7 +9,42 @@ import SwiftUI
 
 struct AlignmrntView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        return ZStack{
+            Rectangle()
+                .frame(width: 2)
+            VStack{
+                Group{
+                    RoundedRectangle(cornerRadius: 20)
+                        .alignmentGuide(HorizontalAlignment.center) { d in d[HorizontalAlignment.center] + 20
+                            //센터 중간에서 왼쪽으로 + 20
+                        }
+                        .alignmentGuide(HorizontalAlignment.center){ d in
+                            let explicit = d[explicit: HorizontalAlignment.center] ?? 0
+                            print("explicit - \(explicit)")
+                            return explicit
+                        }
+                    RoundedRectangle(cornerRadius: 20)
+                        .alignmentGuide(HorizontalAlignment.center){ d in
+                            let explicit = d[explicit: HorizontalAlignment.center] ?? 0
+                            print("explicit - \(explicit)")
+                            return explicit
+                        }
+                    RoundedRectangle(cornerRadius: 20)
+                    RoundedRectangle(cornerRadius: 20)
+                    RoundedRectangle(cornerRadius: 20)
+                        .alignmentGuide(HorizontalAlignment.center, computeValue: { dimension in
+                            20  //센터를 기준으로 잡고 왼쪽으로 - 20
+                        })
+                        .alignmentGuide(HorizontalAlignment.center){ d in
+                            let explicit = d[explicit: HorizontalAlignment.center] ?? 0
+                            print("explicit - \(explicit)")
+                            return explicit
+                        }
+                }
+                .frame(width: 100,height: 50)
+                
+            }
+        }
     }
 }
 
